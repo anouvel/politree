94d71cea57a7abc0dff3a93215e87198a046f11e
Adrien Nouvel <adrien.nouvel@d2-si.eu>
2018-06-15 11:13:37 +0200

    Add the first running version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # policy_tree_generator
 A project to generate a tree of the Vault policies.
+
+## Pre-requesites
+
+Install [Graphviz](http://www.graphviz.org/).
+
+## Run
+
+```
+ruby main.rb examples/default.hcl
+open examples/default.hcl.jpeg
+```
--- main.rb
+++ main.rb
@@ -6,10 +6,41 @@ gemfile true do
   source 'https://rubygems.org'
   gem 'pry', require: true
   gem 'pry-nav', require: true
+  gem 'rhcl', require: true
+  gem 'rgl', require: false
+end
+
+require 'rgl/adjacency'
+require 'rgl/dot'
+
+def build_graph(filename)
+  result = RGL::DirectedAdjacencyGraph.new
+  src_hcl = Rhcl.parse(open(filename).read)
+  for policy in src_hcl do
+    all_path = policy[1]
+    for path in all_path do
+        tmp_path = ""
+        s_path = path[0].split('/')
+        s_path.each_with_index do |item, index|
+          tmp_path = tmp_path + "/" + s_path[index]
+          result.add_vertex tmp_path
+          if index > 0 then
+            result.add_edge tmp_path.rpartition('/')[0],tmp_path
+          end
+        end
+    end
+  end
+  result.write_to_graphic_file(fmt = 'jpeg', dotfile = filename)
 end
 
 def main
-  puts "Hello world !"
+  src = ARGV[0]
+  if src == nil
+    puts "Please provide a filename with policies."
+    exit 1
+  else
+    build_graph(src)
+  end
 end
 
 main

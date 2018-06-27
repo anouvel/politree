d9ee9fdb90c3ffedbd886688e9e517e8996650c8
Clement Hussenot <chussenota@gmail.com>
2018-06-27 11:24:51 +0200

    Remove the generated specs

--- spec/politree_spec.rb
+++ spec/politree_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe Politree do
-  it 'has a version number' do
-    expect(Politree::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
